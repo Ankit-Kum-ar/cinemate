a06d48d2271a87a2c899aee8fba9f7a08f0d1b73
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-30 10:28:11 +0530

    MovieCard Component build using flowbite

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import MovieCard from '../components/MovieCard'
 const MovieList = () => {
   return (
     <main>
-      MovieList
+      <section className='max-w-7xl mx-auto py-7'>
+        <div className='flex justify-start flex-wrap'>
+          <MovieCard/>
+        </div>
+      </section>
     </main>
   )
 }
